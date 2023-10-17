60a827a19a8df0caf834084b201aa1d6311b1f68
NathanCarlini <nathan1.carlini@gmail.com>
2023-10-17 15:20:50 +0200

    :poop: problem with fill of the page

---
+++ orionshalosis/app/signup/page.js
@@ -0,0 +1,30 @@
+export default function Page() {
+
+  return (
+    <div className="flex h-full flex-col items-center justify-center bg-[url('/galaxy.png')] bg-cover bg-no-repeat">
+      <section className="bg-random-grey flex flex-col gap-5 p-8 lg:h-[70vh] lg:w-[45vw]">
+        <h1 className="text-center text-2xl font-bold capitalize text-black">
+          Sign In
+        </h1>
+        <div className="">
+          <p className="text-3xl font-bold">Username :</p>
+          <input type="text" id="Username" className="h-10 w-full"></input>
+        </div>
+        <div>
+          <p className="text-3xl font-bold">Password :</p>
+          <input type="password" id="Password" className="h-10 w-full"></input>
+          <p>You forgot your password ?</p>
+        </div>
+        <div className="mb-6 mt-7 flex grow flex-col justify-evenly">
+          <div className=" max-w-fit self-center rounded-full bg-white px-16 py-2 text-xl font-black capitalize text-black">
+            Sign In
+          </div>
+          <p>Create an account : </p>
+          <div className="max-w-fit self-center rounded-full bg-black px-16 py-2 text-xl font-black capitalize text-white">
+            Sign Up
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
