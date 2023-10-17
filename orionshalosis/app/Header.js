bda453838a4485a1e84971235f958262542d023e
NathanCarlini <nathan1.carlini@gmail.com>
2023-10-17 15:21:49 +0200

    :art: more decomposition

---
+++ orionshalosis/app/Header.js
@@ -0,0 +1,45 @@
+import Image from "next/image";
+import Link from "next/link";
+import "./globals.css";
+
+export default function Header({vis}) {
+  return (
+    <>
+      <header className="flex w-full flex-row items-center justify-between bg-marine-blue px-3 py-[0.10rem] text-xl font-bold">
+        <Link className="flex items-center" href="/">
+          <Image
+            src="/orionsLogo.png"
+            alt="Logo de Orions Halosis"
+            height={75}
+            width={75}
+            className="mr-2 p-0"
+          />
+          Orion's Halosis
+        </Link>
+        <div className="flex flex-row gap-16">
+          <Link className="flex items-center" href="/wiki">
+            Wiki
+          </Link>
+          <Link className="flex items-center" href="/forum">
+            Forum
+          </Link>
+          <Link className="flex items-center" href="/game/overview">
+            The Game
+          </Link>
+        </div>
+        {vis == 1 ? (
+          <div className="flex flex-row gap-3">
+            <div className=" rounded-full bg-black px-8 py-2 capitalize text-white">
+              sign up
+            </div>
+            <div className=" rounded-full bg-white px-8 py-2 capitalize text-black">
+              Log In
+            </div>
+          </div>
+        ) : (
+          <></>
+        )}
+      </header>
+    </>
+  );
+}
--- orionshalosis/app/layout.js
+++ orionshalosis/app/layout.js
@@ -1,46 +1,16 @@
 import "./globals.css";
 import Image from "next/image";
 import Link from "next/link";
+import Header from "./Header";
+import Footer from "./Footer";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-black">
-        <header className="flex w-full flex-row items-center justify-between bg-marine-blue px-3 py-2 text-xl font-bold">
-          <Link className="flex items-center" href="/">
-            <Image
-              src="/orionsLogo.png"
-              alt="Logo de Orions Halosis"
-              height={80}
-              width={80}
-              className="mr-2 p-0"
-            />
-            Orion's Halosis
-          </Link>
-          <div className="flex flex-row gap-16">
-            <Link className="flex items-center" href="/wiki">
-              Wiki
-            </Link>
-            <Link className="flex items-center" href="/forum">
-              Forum
-            </Link>
-            <Link className="flex items-center" href="/game/overview">
-              The Game
-            </Link>
-          </div>
-          <div className="flex flex-row gap-3">
-            <div className=" rounded-full bg-black px-8 py-2 capitalize text-white">
-              sign up
-            </div>
-            <div className=" rounded-full bg-white px-8 py-2 capitalize text-black">
-              Log In
-            </div>
-          </div>
-        </header>
-        <main className="h-full w-full">{children}</main>
-        <footer className="flex w-full flex-row px-4 py-3 bg-darkdark-blue">
-          2023 Orions's Halosis ©
-        </footer>
+      <body className="flex min-h-screen flex-col">
+        <Header vis="1" />
+        <main className="h-full w-full grow">{children}</main>
+        <Footer vis="1" />
       </body>
     </html>
   );
