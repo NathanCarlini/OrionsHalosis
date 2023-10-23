6d6adc53278f9763fec5fc5d0593534729ea3563
SwanYooshy <swanchevallereau@gmail.com>
2023-10-23 14:25:06 +0200

    :sparkles: scale of planets

--- orionshalosis/script.js
+++ orionshalosis/script.js
@@ -34,7 +34,7 @@ rocket.load('./resources/rocketfuture.glb', function ( gltf ) {
     rocketscene = gltf.scene;
     rocketscene.position.y =0;
     gltf.scene.scale.set(0.1, 0.1, 0.1); 
-    gltf.scene.position.set(30, 0.9, 0)
+    gltf.scene.position.set(35, 1.95, 0)
     gltf.scenes;
     gltf.cameras; 
     gltf.asset; 
@@ -47,7 +47,7 @@ plane.rotateX(Math.PI/2)
 // scene.add( plane );
 
 // planets
-const sunGeometry = new THREE.SphereGeometry( 5, 32, 32 );
+const sunGeometry = new THREE.SphereGeometry( 10, 32, 32 );
 const textureSun = new THREE.TextureLoader().load('resources/sun.jpg' ); 
 const sunMaterial = new THREE.MeshPhongMaterial({map: textureSun});
 const sunMesh = new THREE.Mesh(sunGeometry, sunMaterial);
@@ -57,66 +57,67 @@ const MercuryGeometry = new THREE.SphereGeometry( 1, 32, 32 );
 const textureMercury = new THREE.TextureLoader().load('resources/mercury.png' ); 
 const MercuryMaterial = new THREE.MeshPhongMaterial({map: textureMercury});
 const MercuryMesh = new THREE.Mesh(MercuryGeometry, MercuryMaterial);
-MercuryMesh.position.x = 10;
+MercuryMesh.position.x = 15;
 scene.add(MercuryMesh);
 
-const VenusGeometry = new THREE.SphereGeometry( 1, 32, 32 );
+const VenusGeometry = new THREE.SphereGeometry( 1.8, 32, 32 );
 const textureVenus = new THREE.TextureLoader().load('resources/venus.jpg' ); 
 const VenusMaterial = new THREE.MeshPhongMaterial({map: textureVenus});
 const VenusMesh = new THREE.Mesh(VenusGeometry, VenusMaterial);
-VenusMesh.position.x = 20;
+VenusMesh.position.x = 25;
 scene.add(VenusMesh);
 
-const earthGeometry = new THREE.SphereGeometry( 1, 32, 32 );
+const earthGeometry = new THREE.SphereGeometry( 2, 32, 32 );
 const textureEarth = new THREE.TextureLoader().load('resources/earthmap.jpg' ); 
 const textureEarthBump = new THREE.TextureLoader().load('resources/earthbump.jpg' ); 
 const textureEarthSpec = new THREE.TextureLoader().load('resources/earthspec.jpg' ); 
 const earthMaterial = new THREE.MeshPhongMaterial({map: textureEarth, bumpMap: textureEarthBump, specular: textureEarthSpec});
 const earthMesh = new THREE.Mesh(earthGeometry, earthMaterial);
-earthMesh.position.x = 30;
+earthMesh.position.x = 35;
 scene.add(earthMesh);
 
-const MarsGeometry = new THREE.SphereGeometry( 1, 32, 32 );
+const MarsGeometry = new THREE.SphereGeometry( 1.4, 32, 32 );
 const textureMars = new THREE.TextureLoader().load('resources/mars.jpg' ); 
 const MarsMaterial = new THREE.MeshPhongMaterial({map: textureMars});
 const MarsMesh = new THREE.Mesh(MarsGeometry, MarsMaterial);
-MarsMesh.position.x = 40;
+MarsMesh.position.x = 45;
 scene.add(MarsMesh);
 
-const JupiterGeometry = new THREE.SphereGeometry( 2, 32, 32 );
+const JupiterGeometry = new THREE.SphereGeometry( 5, 32, 32 );
 const textureJupiter = new THREE.TextureLoader().load('resources/jupiter.jpg' ); 
 const JupiterMaterial = new THREE.MeshPhongMaterial({map: textureJupiter});
 const JupiterMesh = new THREE.Mesh(JupiterGeometry, JupiterMaterial);
-JupiterMesh.position.x = 50;
+JupiterMesh.position.x = 60;
 scene.add(JupiterMesh);
 
-const SaturnGeometry = new THREE.SphereGeometry( 1, 32, 32 );
+const SaturnGeometry = new THREE.SphereGeometry( 4.5, 32, 32 );
 const textureSaturn = new THREE.TextureLoader().load('resources/saturn.jpg' ); 
 const SaturnMaterial = new THREE.MeshPhongMaterial({map: textureSaturn});
 const SaturnMesh = new THREE.Mesh(SaturnGeometry, SaturnMaterial);
-SaturnMesh.position.x = 60;
+SaturnMesh.position.x = 75;
 scene.add(SaturnMesh);
 
-const SaturnRingGeometry = new THREE.RingGeometry( 3, 5, 32 ); 
-const textureSaturnRing = new THREE.TextureLoader().load('resources/saturn_ring.png' ); 
-const SaturnRingMaterial = new THREE.MeshPhongMaterial( { map: textureSaturnRing, side: THREE.DoubleSide } );
+const SaturnRingGeometry = new THREE.RingGeometry( 5, 8, 64 ); 
+const SaturnRingMaterial = new THREE.MeshPhongMaterial( { color: 0xFFFFFF, side: THREE.DoubleSide } );
+SaturnRingMaterial.transparent = true;
+SaturnRingMaterial.opacity = 0.2;
 const SaturnRingMesh = new THREE.Mesh( SaturnRingGeometry, SaturnRingMaterial ); 
-SaturnRingMesh.position.x = 50;
+SaturnRingMesh.position.x = 75;
 SaturnRingMesh.rotateX(Math.PI/2);
 scene.add( SaturnRingMesh );
 
-const UranusGeometry = new THREE.SphereGeometry( 1, 32, 32 );
+const UranusGeometry = new THREE.SphereGeometry( 3.5, 32, 32 );
 const textureUranus = new THREE.TextureLoader().load('resources/uranus.jpg' ); 
 const UranusMaterial = new THREE.MeshPhongMaterial({map: textureUranus});
 const UranusMesh = new THREE.Mesh(UranusGeometry, UranusMaterial);
-UranusMesh.position.x = 70;
+UranusMesh.position.x = 90;
 scene.add(UranusMesh);
 
-const NeptuneGeometry = new THREE.SphereGeometry( 1, 32, 32 );
+const NeptuneGeometry = new THREE.SphereGeometry( 3.1, 32, 32 );
 const textureNeptune = new THREE.TextureLoader().load('resources/neptune.jpg' ); 
 const NeptuneMaterial = new THREE.MeshPhongMaterial({map: textureNeptune});
 const NeptuneMesh = new THREE.Mesh(NeptuneGeometry, NeptuneMaterial);
-NeptuneMesh.position.x = 80;
+NeptuneMesh.position.x = 100;
 scene.add(NeptuneMesh);
 
 let light = new THREE.DirectionalLight(0xFFFFFF, 1.0);
