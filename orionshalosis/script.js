666aedb40cefdc06aa5425bc1a11dd9a336cf787
SwanYooshy <swanchevallereau@gmail.com>
2023-10-23 11:07:30 +0200

    :sparkles: all planets of the solar system

--- orionshalosis/script.js
+++ orionshalosis/script.js
@@ -33,8 +33,8 @@ rocket.load('./resources/rocketfuture.glb', function ( gltf ) {
     gltf.animations;
     rocketscene = gltf.scene;
     rocketscene.position.y =0;
-    gltf.scene.scale.set(0.2, 0.2, 0.2); 
-    gltf.scene.position.set(15, 1.75, 0)
+    gltf.scene.scale.set(0.1, 0.1, 0.1); 
+    gltf.scene.position.set(30, 0.9, 0)
     gltf.scenes;
     gltf.cameras; 
     gltf.asset; 
@@ -53,25 +53,74 @@ const sunMaterial = new THREE.MeshPhongMaterial({map: textureSun});
 const sunMesh = new THREE.Mesh(sunGeometry, sunMaterial);
 scene.add(sunMesh);
 
-const earthGeometry = new THREE.SphereGeometry( 2, 32, 32 );
+const MercuryGeometry = new THREE.SphereGeometry( 1, 32, 32 );
+const textureMercury = new THREE.TextureLoader().load('resources/mercury.png' ); 
+const MercuryMaterial = new THREE.MeshPhongMaterial({map: textureMercury});
+const MercuryMesh = new THREE.Mesh(MercuryGeometry, MercuryMaterial);
+MercuryMesh.position.x = 10;
+scene.add(MercuryMesh);
 
+const VenusGeometry = new THREE.SphereGeometry( 1, 32, 32 );
+const textureVenus = new THREE.TextureLoader().load('resources/venus.jpg' ); 
+const VenusMaterial = new THREE.MeshPhongMaterial({map: textureVenus});
+const VenusMesh = new THREE.Mesh(VenusGeometry, VenusMaterial);
+VenusMesh.position.x = 20;
+scene.add(VenusMesh);
+
+const earthGeometry = new THREE.SphereGeometry( 1, 32, 32 );
 const textureEarth = new THREE.TextureLoader().load('resources/earthmap.jpg' ); 
 const textureEarthBump = new THREE.TextureLoader().load('resources/earthbump.jpg' ); 
 const textureEarthSpec = new THREE.TextureLoader().load('resources/earthspec.jpg' ); 
 const earthMaterial = new THREE.MeshPhongMaterial({map: textureEarth, bumpMap: textureEarthBump, specular: textureEarthSpec});
 const earthMesh = new THREE.Mesh(earthGeometry, earthMaterial);
-earthMesh.position.x = 15;
+earthMesh.position.x = 30;
 scene.add(earthMesh);
 
+const MarsGeometry = new THREE.SphereGeometry( 1, 32, 32 );
+const textureMars = new THREE.TextureLoader().load('resources/mars.jpg' ); 
+const MarsMaterial = new THREE.MeshPhongMaterial({map: textureMars});
+const MarsMesh = new THREE.Mesh(MarsGeometry, MarsMaterial);
+MarsMesh.position.x = 40;
+scene.add(MarsMesh);
+
+const JupiterGeometry = new THREE.SphereGeometry( 2, 32, 32 );
+const textureJupiter = new THREE.TextureLoader().load('resources/jupiter.jpg' ); 
+const JupiterMaterial = new THREE.MeshPhongMaterial({map: textureJupiter});
+const JupiterMesh = new THREE.Mesh(JupiterGeometry, JupiterMaterial);
+JupiterMesh.position.x = 50;
+scene.add(JupiterMesh);
+
+const SaturnGeometry = new THREE.SphereGeometry( 1, 32, 32 );
+const textureSaturn = new THREE.TextureLoader().load('resources/saturn.jpg' ); 
+const SaturnMaterial = new THREE.MeshPhongMaterial({map: textureSaturn});
+const SaturnMesh = new THREE.Mesh(SaturnGeometry, SaturnMaterial);
+SaturnMesh.position.x = 60;
+scene.add(SaturnMesh);
+
+const UranusGeometry = new THREE.SphereGeometry( 1, 32, 32 );
+const textureUranus = new THREE.TextureLoader().load('resources/uranus.jpg' ); 
+const UranusMaterial = new THREE.MeshPhongMaterial({map: textureUranus});
+const UranusMesh = new THREE.Mesh(UranusGeometry, UranusMaterial);
+UranusMesh.position.x = 70;
+scene.add(UranusMesh);
+
+const NeptuneGeometry = new THREE.SphereGeometry( 1, 32, 32 );
+const textureNeptune = new THREE.TextureLoader().load('resources/neptune.jpg' ); 
+const NeptuneMaterial = new THREE.MeshPhongMaterial({map: textureNeptune});
+const NeptuneMesh = new THREE.Mesh(NeptuneGeometry, NeptuneMaterial);
+NeptuneMesh.position.x = 80;
+scene.add(NeptuneMesh);
+
 let light = new THREE.DirectionalLight(0xFFFFFF, 1.0);
-light.position.set(20, 80, 10);
+let light2 = new THREE.AmbientLight(0xFFFFFF, 1);
+light.position.set(20, 60, 10);
 light.target.position.set(0, 0, 0);
 light.castShadow = true;
-scene.add(light);
+scene.add(light, light2);
 
 const camera = new THREE.PerspectiveCamera(45,
     sizes.width / sizes.height);
-camera.position.set(75, 20, 0);
+camera.position.set(100, 20, 0);
 camera.lookAt(0, 0, 0);
 scene.add(camera);
 
