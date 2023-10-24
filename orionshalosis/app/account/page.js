2e521188a902d4285876fcff527345dcc8dcd6ec
NathanCarlini <nathan1.carlini@gmail.com>
2023-10-24 11:54:39 +0200

    :construction: onwork, data retrieve needed

--- orionshalosis/app/account/page.js
+++ orionshalosis/app/account/page.js
@@ -3,10 +3,10 @@ import Link from "next/link";
 
 export default function Page() {
   return (
-    <div className="absolute flex h-full w-full flex-col bg-[url('/bg.png')] p-11">
+    <div className="absolute flex h-full w-full flex-col bg-[url('/backgrounds/bg.png')] p-11">
       <section className="flex flex-row justify-between">
         <div className="flex flex-row gap-3">
-          <div className="aspect-square bg-slate-400/40 w-40 h-40 relative">
+          <div className="relative aspect-square h-40 w-40 bg-slate-400/40">
             <Image
               src="/defaultuser.png"
               layout="fill"
@@ -14,21 +14,46 @@ export default function Page() {
               alt="user avatar"
             ></Image>
           </div>
-          <div className="flex flex-col h-full gap-4">
+          <div className="flex h-full flex-col gap-4">
             <p className="bg-slate-400/40 px-4 py-[6px] text-xl font-black text-white">
               Name of User
             </p>
             <p className="bg-slate-400/40 px-4 py-[6px] text-xl font-black text-white">
               Beginner space Explorer lvl.2
             </p>
-            <div className="w-full rounded-full h-11 bg-[url('/galaxy.png')]"></div>
+            <div className="h-11 w-full rounded-full bg-[url('/backgrounds/galaxy.png')]"></div>
           </div>
         </div>
-        <button className="rounded-full bg-main-blue text-white font-bold text-lg py-2 px-4 h-fit ">My Statistics</button>
+        <button className="h-fit rounded-full bg-main-blue px-4 py-2 text-lg font-bold text-white ">
+          My Statistics
+        </button>
       </section>
-      <section className="flex flex-row mb-6 justify-evenly mt-12">
-        <div className="h-full bg-slate-400/40 py-10 px-16"><p className="text-3xl font-bold">Your infos</p></div>
-        <div className="h-full bg-slate-400/40 py-10 px-16"><p className="text-3xl font-bold">Inventory</p></div>
+      <section className="mb-6 mt-12 flex flex-row justify-evenly gap-11">
+        <div className="flex h-full flex-col bg-slate-400/40 px-16 py-10 w-full ">
+          <p className="text-3xl font-bold text-center">Your infos</p>
+          <form className="flex flex-col">
+            <label className="mb-3" htmlFor="username">Username :</label>
+            <input className="bg-white text-black h-10 w-full" id="username" name="username" type="text" ></input>
+
+            <label className="mt-5 mb-3" htmlFor="">Your e-mail :</label>
+            <input className="bg-white text-black h-10 w-full" id="email" name="email" type="email" ></input>
+
+            <label className="mt-5 mb-3" htmlFor="">Current password :</label>
+            <input className="bg-white text-black h-10 w-full" id="password" name="password" type="password" ></input>
+
+            <label className="mt-5 mb-3" htmlFor="">New password :</label>
+            <input className="bg-white text-black h-10 w-full mb-8" id="" name="" type="text"></input>
+          </form>
+        </div>
+        <div className="flex h-full flex-col bg-slate-400/40 px-16 py-10 w-full">
+          <p className="text-3xl font-bold">Inventory</p>
+          <div>
+            <p className="text-black font-black text-xl">Spacecraft</p>
+            <div className="flex flex-row">
+              
+            </div>
+          </div>
+        </div>
       </section>
     </div>
   );
