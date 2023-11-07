8d2c891fa81e6a116f3c1546cf43dbbfca7128b0
NathanCarlini <nathan1.carlini@gmail.com>
2023-11-07 08:08:04 +0100

    :sparkles: added item inventory render

--- orionshalosis/app/ItemRenderLoop.js
+++ orionshalosis/app/ItemRenderLoop.js
@@ -1,15 +1,20 @@
-'use client'
-export default function ItemRenderLoop(array) {
-  for (let item of array) {
-    return (
-        <>
+"use client";
 
-      <div style={{ background: item.image }} className="item max-w-24 aspect-square max-h-24 bg-cover bg-no-repeat"  onmouseover="" />
+import Image from "next/image";
 
-
-      </>
-    );
-  }
-
-  7;
+export default function ItemRenderLoop(item) {
+  return (
+    <div
+      // style={{ background: url(`${item.image}`) }}
+      className="max-w-24 relative aspect-square max-h-24 bg-cover bg-no-repeat"
+    >
+      <Image
+        src={item.image}
+        className="z-1 absolute left-0 top-0"
+        height={200}
+        width={200}
+      />
+      <p className="z-2 text-white">item.name</p>
+    </div>
+  );
 }
