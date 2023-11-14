c6e25e1028737525ecf5a11b71bfc4c7abab3adf
NathanCarlini <nathan1.carlini@gmail.com>
2023-11-14 15:44:57 +0100

    added logout

--- orionshalosis/app/account/page.js
+++ orionshalosis/app/account/page.js
@@ -1,9 +1,31 @@
+"use client"
 import Image from "next/image";
 import Link from "next/link";
 import ItemRenderLoop from "../ItemRenderLoop";
 
+function LogOut(){
+  fetch("http://localhost:8080/logOut", { method:"PUT"})
+}
+
 export default function Page() {
-  let test = [{image: "https://static1.millenium.org/entity_articles/5/12/95/@/238471-classique-gratuit-article_m-2.png", name:"test"}, {image: "https://static1.millenium.org/entity_articles/5/12/95/@/238471-classique-gratuit-article_m-2.png", name:"test"}, {image: "https://static1.millenium.org/entity_articles/5/12/95/@/238471-classique-gratuit-article_m-2.png", name:"test"}]
+  let test = [
+    {
+      image:
+        "https://static1.millenium.org/entity_articles/5/12/95/@/238471-classique-gratuit-article_m-2.png",
+      name: "test",
+    },
+    {
+      image:
+        "https://static1.millenium.org/entity_articles/5/12/95/@/238471-classique-gratuit-article_m-2.png",
+      name: "test",
+    },
+    {
+      image:
+        "https://static1.millenium.org/entity_articles/5/12/95/@/238471-classique-gratuit-article_m-2.png",
+      name: "test",
+    },
+  ];
+
   return (
     <div className="absolute flex h-full w-full flex-col bg-[url('/backgrounds/bg.png')] p-11">
       <section className="flex flex-row justify-between">
@@ -26,39 +48,71 @@ export default function Page() {
             <div className="h-11 w-full rounded-full bg-[url('/backgrounds/galaxy.png')]"></div>
           </div>
         </div>
-        <button className="h-fit rounded-full bg-main-blue px-4 py-2 text-lg font-bold text-white ">
-          My Statistics
-        </button>
+        <div className="flex flex-col gap-5">
+          <button className="h-fit rounded-full bg-main-blue px-4 py-2 text-lg font-bold text-white ">
+            My Statistics
+          </button>
+          <Link
+            className="h-fit rounded-full bg-red-500 px-4 py-2 text-center text-lg font-bold text-white"
+            href="/" onClick={LogOut}
+          >
+            Log Out
+          </Link>
+        </div>
       </section>
       <section className="mb-6 mt-12 flex flex-row justify-evenly gap-11">
-        <div className="flex h-full flex-col bg-slate-400/40 px-16 py-10 w-full ">
-          <p className="text-3xl font-bold text-center">Your infos</p>
+        <div className="flex h-full w-full flex-col bg-slate-400/40 px-16 py-10 ">
+          <p className="text-center text-3xl font-bold">Your infos</p>
           <form className="flex flex-col">
-            <label className="mb-3" htmlFor="username">Username :</label>
-            <input className="bg-white text-black h-10 w-full" id="username" name="username" type="text" ></input>
+            <label className="mb-3" htmlFor="username">
+              Username :
+            </label>
+            <input
+              className="h-10 w-full bg-white text-black"
+              id="username"
+              name="username"
+              type="text"
+            ></input>
 
-            <label className="mt-5 mb-3" htmlFor="">Your e-mail :</label>
-            <input className="bg-white text-black h-10 w-full" id="email" name="email" type="email" ></input>
+            <label className="mb-3 mt-5" htmlFor="">
+              Your e-mail :
+            </label>
+            <input
+              className="h-10 w-full bg-white text-black"
+              id="email"
+              name="email"
+              type="email"
+            ></input>
 
-            <label className="mt-5 mb-3" htmlFor="">Current password :</label>
-            <input className="bg-white text-black h-10 w-full" id="password" name="password" type="password" ></input>
+            <label className="mb-3 mt-5" htmlFor="">
+              Current password :
+            </label>
+            <input
+              className="h-10 w-full bg-white text-black"
+              id="password"
+              name="password"
+              type="password"
+            ></input>
 
-            <label className="mt-5 mb-3" htmlFor="">New password :</label>
-            <input className="bg-white text-black h-10 w-full mb-8" id="" name="" type="text"></input>
+            <label className="mb-3 mt-5" htmlFor="">
+              New password :
+            </label>
+            <input
+              className="mb-8 h-10 w-full bg-white text-black"
+              id=""
+              name=""
+              type="text"
+            ></input>
           </form>
         </div>
-        <div className="flex h-full flex-col bg-slate-400/40 px-16 py-10 w-full">
-
+        <div className="flex h-full w-full flex-col bg-slate-400/40 px-16 py-10">
           <p className="text-3xl font-bold">Inventory</p>
           <div>
-            <p className="text-black font-black text-xl">Spacecraft</p>
+            <p className="text-xl font-black text-black">Spacecraft</p>
             <div className="flex flex-row">
               <ItemRenderLoop array={test} />
             </div>
           </div>
-
-
-
         </div>
       </section>
     </div>
