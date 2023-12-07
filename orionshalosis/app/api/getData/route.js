855d96fbf4776aca1cf968e66364b1749bd7664f
NathanCarlini <nathan1.carlini@gmail.com>
2023-12-07 08:35:07 +0100

    :sparkles: added new Endpoint

--- orionshalosis/app/api/getData/route.js
+++ orionshalosis/app/api/getData/route.js
@@ -3,8 +3,9 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export async function GET(request) {
+export async function PUT(request) {
   const body = await request.json();
+  console.log(body);
   const res = await prisma.account.findUnique({
     where: {
       email: body.email,
