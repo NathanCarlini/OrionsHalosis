854e725833ef6890f6eb66c9b9abdff43f97a4d3
NathanCarlini <nathan1.carlini@gmail.com>
2023-12-11 16:31:44 +0100

    :sparkles: added new page

---
+++ orionshalosis/app/forum/page.js
@@ -0,0 +1,7 @@
+function page() {
+  return <div className="flex h-full w-full flex-col">
+    <h1 className="text-2xl font-black text-white text-center"> Welcome to the Forum</h1>
+  </div>;
+}
+
+export default page;
