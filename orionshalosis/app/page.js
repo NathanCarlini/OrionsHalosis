b4f440ed49d9da9c82e254a9d39cc2cdf6029c3a
SwanYooshy <swanchevallereau@gmail.com>
2023-12-12 09:59:25 +0100

    font ignore

--- orionshalosis/app/page.js
+++ orionshalosis/app/page.js
@@ -1,9 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
 
-// import "./globals.css";
-// import "./font/SpaceMono-Regular.ttf";
-
 export default function Home() {
   return (
     <>
