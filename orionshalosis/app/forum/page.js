854faf5b0a050b957488c4a28e1accf8b3383934
NathanCarlini <nathan1.carlini@gmail.com>
2023-12-12 09:05:00 +0100

    onWork

--- orionshalosis/app/forum/page.js
+++ orionshalosis/app/forum/page.js
@@ -1,6 +1,6 @@
 function page() {
   return <div className="flex h-full w-full flex-col">
-    <h1 className="text-2xl font-black text-white text-center"> Welcome to the Forum</h1>
+    <h1 className="text-5xl font-black text-white text-center"> Welcome to the Forum</h1>
   </div>;
 }
 
