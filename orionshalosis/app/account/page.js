327a388b4f2942b9ff96773a0f80bf033c6a24a0
NathanCarlini <nathan1.carlini@gmail.com>
2023-12-21 17:15:04 +0100

    getata

--- orionshalosis/app/account/page.js
+++ orionshalosis/app/account/page.js
@@ -23,7 +23,7 @@ export default async function Page() {
     }
     const validateToken = async () => {
       try {
-        const res = await fetch(`/api/getData`,
+        const res = await fetch(`/api/getata`,
           {
             method: "GET",
             headers: { Authorization: `Bearer ${token}` },
