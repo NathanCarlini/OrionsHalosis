c8b51b571a29a5a505980df8e6be7d0f52cfdb77
NathanCarlini <nathan1.carlini@gmail.com>
2024-01-11 11:29:59 +0100

    :sparkles: pwd encryption working

--- orionshalosis/app/api/loginUsr/route.js
+++ orionshalosis/app/api/loginUsr/route.js
@@ -1,31 +1,49 @@
 import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
 import jwt from "jsonwebtoken";
-
+const bcrypt = require("bcrypt")
 
 const prisma = new PrismaClient();
 
 export async function PUT(request) {
   const body = await request.json();
-  const res = await prisma.account.findFirst ({
+  const res = await prisma.account.findFirst({
     where: {
       username: body.username,
     },
   });
-  console.log(res);
-  if (body.password == res.password) {
-    console.log("tetetetete");
-    const token = jwt.sign({ userId: res.username }, process.env.JWT_SECRET, {
-      expiresIn: "60m",
-    });
-    console.log(token);
-    return NextResponse.json({ token });
-  } else {
-    return NextResponse.json(
-      { message: "Email or password not valid" },
-      {
-        status: 400,
-      },
-    );
-  }
+
+  // if (body.password == res.password) {
+  //   const token = jwt.sign({ userId: res.username }, process.env.JWT_SECRET, {
+  //     expiresIn: "60m",
+  //   });
+  //   return NextResponse.json({ token });
+  // } else {
+  //   return NextResponse.json(
+  //     { message: "Email or password not valid" },
+  //     {
+  //       status: 400,
+  //     },
+  //   );
+  // }
+  console.log(body.password);
+  console.log(res.password);
+  bcrypt
+    .compare(body.password, res.password)
+    .then((res) => {
+      if (res == true) {
+        const token = jwt.sign({ userId: res.username }, process.env.JWT_SECRET, {
+          expiresIn: "60m",
+        });
+        return NextResponse.json({ token });
+      } else {
+        return NextResponse.json(
+          { message: "Email or password not valid" },
+          {
+            status: 400,
+          },
+        );
+      }
+    })
+    .catch((err) => console.error(err.message));
 }
