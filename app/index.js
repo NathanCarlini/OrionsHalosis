abaeaa7b359588e1a70e02a8eda8c28e8c0fb588
SwanYooshy <swanchevallereau@gmail.com>
2024-01-17 16:31:27 +0100

    test

--- app/index.js
+++ app/index.js
@@ -11,10 +11,7 @@ const httpsServer = https.createServer({
 // const windowPath = window.location.origin;
 const io = new Server(httpsServer, {
   cors: {
-    origin: `https://planets.mmi-limoges.fr:3000`,
-    methods: ["GET", "POST"],
-    allowedHeaders: [""],
-    credentials: true,
+    origin: "*"
   },
 });
 let room = "";
